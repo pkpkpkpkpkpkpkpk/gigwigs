ed8565a03e7148aeddcc41354851d3246f64ae69
pkpkpkpkpkpkpkpk <pk.2121@gmail.com>
2019-06-13 22:21:10 +1000

    initial cleanup

---
+++ src/components/Gig/Gig.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './Gig.module.css';
+
+const Gig = props => (
+  null
+);
+
+export default Gig;
---
+++ src/containers/Gigs/Gigs.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './Gigs.module.css';
+
+const Gigs = props => (
+  null
+);
+
+export default Gigs;
