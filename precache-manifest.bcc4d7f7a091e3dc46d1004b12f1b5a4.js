1894ce5ebb6122fc7f96e5acfddc585202fd2051
pkpkpkpkpkpkpkpk <pk.2121@gmail.com>
2019-06-24 13:43:05 +1000

    Updates

---
+++ precache-manifest.bcc4d7f7a091e3dc46d1004b12f1b5a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f5bf96949244fcb93d8d00a638d9b7c",
+    "url": "/gigwigs/index.html"
+  },
+  {
+    "revision": "000b3d301bce5458d652",
+    "url": "/gigwigs/static/css/2.c51b6406.chunk.css"
+  },
+  {
+    "revision": "4ac6390823339ce6bc79",
+    "url": "/gigwigs/static/css/main.91b2ef2d.chunk.css"
+  },
+  {
+    "revision": "000b3d301bce5458d652",
+    "url": "/gigwigs/static/js/2.e8e5155f.chunk.js"
+  },
+  {
+    "revision": "4ac6390823339ce6bc79",
+    "url": "/gigwigs/static/js/main.4820f113.chunk.js"
+  },
+  {
+    "revision": "9f3c84276226d81b893c",
+    "url": "/gigwigs/static/js/runtime~main.086ea278.js"
+  },
+  {
+    "revision": "be70d568be749d72bc08fe398002e0b3",
+    "url": "/gigwigs/static/media/gig-image.be70d568.jpg"
+  }
+]);
